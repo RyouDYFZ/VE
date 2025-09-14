8bf5f54b4203d446d5eb5cb29764fa290e6d225f
DannyFeng <FengzihangDanny@iCloud.com>
2025-09-14 14:30:53 +0800

    Initial Commit

---
+++ VisualEffectBridge/VisualEffectBridge.h
@@ -0,0 +1,12 @@
+//
+//  VisualEffectBridge.h
+//  VisualEffectBridge
+//
+//  Created by 冯子航 on 2025/9/14.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VisualEffectBridge : NSObject
+
+@end
---
+++ VisualEffectBridge/VisualEffectBridge.m
@@ -0,0 +1,12 @@
+//
+//  VisualEffectBridge.m
+//  VisualEffectBridge
+//
+//  Created by 冯子航 on 2025/9/14.
+//
+
+#import "VisualEffectBridge.h"
+
+@implementation VisualEffectBridge
+
+@end
